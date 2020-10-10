fed887ffde55cf63b5a69f06c9479e27651e92d5
andrersp <rsp.assistencia@gmail.com>
2020-10-09 22:42:02 -0300

    init

---
+++ build/CMakeFiles/ERP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ERP_autogen"
+  "CMakeFiles/ERP_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/ERP.dir/main.cpp.o"
+  "CMakeFiles/ERP.dir/ERP_autogen/mocs_compilation.cpp.o"
+  "ERP.pdb"
+  "ERP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ERP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
